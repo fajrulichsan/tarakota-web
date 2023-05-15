f1fcce4fdfc6f2997a8c62e866c13eea5e80fcb1
fajrulichsan <fajrul.ichsankamil@ai.astra.co.id>
2023-05-15 21:16:41 +0700

    test 2

--- src/User/ErrorPage/ErrorComponent.jsx
+++ src/User/ErrorPage/ErrorComponent.jsx
@@ -9,6 +9,7 @@ const ErrorComponent = () => {
         <h1>Oops!</h1>
         <p>Sorry, an asdasd unexpected error has occurred.</p>
         <p>ubah 1</p>
+        <p>ubah 2</p>
         <p>
             <i>{error.statusText || error.message}</i>
         </p>
