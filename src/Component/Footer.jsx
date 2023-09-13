3bfe12856fd007b9db39fe212ad540daf74c3846
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2023-09-13 14:40:44 +0700

    fix : footer sub menu

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,122 +1,157 @@
-import React from 'react';
-import { facebook, twitter, youtube, instagram } from "../assets/img/index";
+import React from "react";
+import {
+  facebookWhite,
+  twitterWhite,
+  youtubeWhite,
+  instagramWhite,
+  logo,
+} from "../assets/img/index";
+
+const productList = [
+  {
+    title: "Proyek",
+    href: "#",
+  },
+  {
+    title: "Estimasi",
+    href: "#",
+  },
+  {
+    title: "Kerjasama",
+    href: "#",
+  },
+  {
+    title: "Testimoni",
+    href: "#",
+  },
+];
+
+const customerServiceList = [
+  {
+    title : "+62 852 8006 1520"
+  },{
+    title : "web@tarakota.com"
+  }
+]
+
+const aboutList = [
+  {
+    title : "Perusahaan",
+    href : "#"
+  },{
+    title : "Klien",
+    href : "#"
+  },{
+    title : "Layanan",
+    href : "#"
+  },{
+    title : "Video Profil",
+    href : "#"
+  }
+]
+
+const sosialMediaIcon = [
+  {
+    icon : facebookWhite,
+    href : "#"
+  },
+  {
+    icon : instagramWhite,
+    href : "#"
+  },
+  {
+    icon : twitterWhite,
+    href : "#"
+  },
+  {
+    icon : youtubeWhite,
+    href : "#"
+  }
+]
 
 const Footer = () => {
-  const socialMediaIcon = [
-    facebook,
-    twitter,
-    youtube,
-    instagram,
-  ];
+
   return (
-    <footer className="bg-gray-800 h-[25em] text-white py-8">
-      <div className="container mx-auto flex flex-wrap justify-between">
-        {/* Subkategori Tambahan */}
-        <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
-          <h3 className="text-2xl font-bold mb-4">All Product</h3>
-          <ul>
-            <li className="mb-2">
-              <a href="#" className="text-lg text-gray-400 hover:text-white">
-                Product 1
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="#" className="text-lg text-gray-400 hover:text-white">
-                Product 2
-              </a>
-            </li>
-            {/* Tambahkan item produk lainnya sesuai kebutuhan */}
-          </ul>
-        </div>
-        <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
-          <h3 className="text-2xl font-bold mb-4">Customer Service</h3>
-          <ul>
-            <li className="mb-2">
-              <a href="#" className="text-lg text-gray-400 hover:text-white">
-                Layanan Pelanggan 1
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="#" className="text-lg text-gray-400 hover:text-white">
-                Layanan Pelanggan 2
-              </a>
-            </li>
-            {/* Tambahkan layanan pelanggan lainnya sesuai kebutuhan */}
-          </ul>
-        </div>
-        <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
-          <h3 className="text-2xl font-bold mb-4">About</h3>
-          <ul>
-            <li className="mb-2">
-              <a href="#" className="text-lg text-gray-400 hover:text-white">
-                Tentang Kami 1
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="#" className="text-lg text-gray-400 hover:text-white">
-                Tentang Kami 2
-              </a>
-            </li>
-            {/* Tambahkan informasi tentang kami lainnya sesuai kebutuhan */}
-          </ul>
-        </div>
-        <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
-          <h3 className="text-2xl font-bold mb-4">Inspiration</h3>
-          <ul>
-            <li className="mb-2">
-              <a href="#" className="text-lg text-gray-400 hover:text-white">
-                Inspirasi 1
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="#" className="text-lg text-gray-400 hover:text-white">
-                Inspirasi 2
-              </a>
-            </li>
-            {/* Tambahkan inspirasi lainnya sesuai kebutuhan */}
-          </ul>
-        </div>
-        <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
-          <h3 className="text-2xl font-bold mb-4">Follow Us</h3>
-          <div className="flex space-x-4">
-            {/* <a
-              href="#"
-              className="text-gray-400 hover:text-white transition duration-300"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a
-              href="#"
-              className="text-gray-400 hover:text-white transition duration-300"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a
-              href="#"
-              className="text-gray-400 hover:text-white transition duration-300"
-            >
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a
-              href="#"
-              className="text-gray-400 hover:text-white transition duration-300"
-            >
-              <i className="fab fa-linkedin"></i>
-            </a> */}
-            {socialMediaIcon.map((icon, index) => (
+    <footer className="bg-gray-800 relative h-[25em]">
+      <div
+        className="absolute inset-0 h-[25em] text-white p-24"
+        style={{
+          borderRadius: "100px 0px 0px 0px",
+          background:
+            "linear-gradient(116deg, #E85738 -14.86%, rgba(0, 0, 0, 0.00) 20.3%)",
+        }}
+      >
+        <img src={logo}></img>
+        <div className="mx-auto flex flex-wrap justify-evenly mt-10">
+          <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
+            <h3 className="text-2xl font-bold mb-4">Produk</h3>
+            <ul>
+              {productList.map((data, index) => (
+                <li key={index} className="mb-2">
+                  <a
+                    href={data.href}
+                    className="text-lg text-gray-400 hover:text-white"
+                  >
+                    {data.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
+            <h3 className="text-2xl font-bold mb-4">Customer Service</h3>
+            <ul>
+            {customerServiceList.map((data, index) => (
+                <li key={index} className="mb-2">
+                  <a
+                    href={data.href}
+                    className="text-lg text-gray-400 hover:text-white"
+                  >
+                    {data.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
+            <h3 className="text-2xl font-bold mb-4">Tentang</h3>
+            <ul>
+            {aboutList.map((data, index) => (
+                <li key={index} className="mb-2">
+                  <a
+                    href={data.href}
+                    className="text-lg text-gray-400 hover:text-white"
+                  >
+                    {data.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/5 px-4 mb-8">
+            <h3 className="text-2xl font-bold mb-4">Follow Us</h3>
+            <div className="flex space-x-4">
+               {sosialMediaIcon.map((data, index) => (
+                <a
+                  key={index}
+                  href={data.href}
+                  className="text-gray-400 hover:text-white transition duration-300"
+                >
                   <img
-                    key={index}
-                    src={icon}
+                    src={data.icon}
                     alt={`Social Media Icon ${index}`}
                     className="w-6"
                   />
-                ))}
+                </a>
+              ))}
+            </div>
           </div>
         </div>
+        <hr></hr>
+
       </div>
     </footer>
   );
 };
 
 export default Footer;
-
--- src/assets/img/index.jsx
+++ src/assets/img/index.jsx
@@ -2,13 +2,23 @@
 
 // feat : slider banner
 export {default as bgBannerSlider} from './bg-banner-slider.svg';
-export {default as facebook} from './facebook.svg';
-export {default as instagram} from './instagram.svg';
-export {default as twitter} from './twiter.svg';
-export {default as youtube} from './youtube.svg';
+
 
 // feat : testimoni
 export {default as profileTestimoni} from './profile-testimoni.svg';
 export {default as iconTestimoni} from './icon-testimoni.svg';
 export {default as previousButton} from './previous-button.svg';
 export {default as nextButton} from './next-button.svg';
+
+// feat : footer
+export {default as logo} from './logo.svg';
+export {default as facebookWhite} from './facebook-white.svg';
+export {default as twitterWhite} from './twiter-white.svg';
+export {default as instagramWhite} from './instagram-white.svg';
+export {default as youtubeWhite} from './youtube-white.svg';
+
+// sosial media
+export {default as facebook} from './facebook.svg';
+export {default as instagram} from './instagram.svg';
+export {default as twitter} from './twiter.svg';
+export {default as youtube} from './youtube.svg';
