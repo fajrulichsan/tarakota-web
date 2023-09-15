682f4bf63c88386c421d88b620e4854ec0d31c9c
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2023-09-15 15:08:23 +0700

    fix : route

--- src/User/pages/LandingPage.jsx
+++ src/User/pages/LandingPage.jsx
@@ -1,11 +1,9 @@
 import { Fragment } from "react"
 import Footer from "../../Component/Footer"
 import Navbar from "../../Component/Navbar"
-import Carousel from "../LandingPage/Carousel"
 import JoinUs from "../LandingPage/JoinUs"
 import OurClient from "../LandingPage/OurClient"
 import ProfileVideo from "../LandingPage/ProfileVideo"
-import ProjectSlider from "../LandingPage/ProjectSlider"
 import SliderBanner from "../LandingPage/SliderBanner"
 import Testimoni from "../LandingPage/Testimoni"
 
