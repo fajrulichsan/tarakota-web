4bb18a661360d4af7f8d12f4a289376c0a99af72
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-02-22 18:11:31 +0700

    fix : bangun page

--- src/User/BangunPage/Introduction.jsx
+++ src/User/BangunPage/Introduction.jsx
@@ -7,11 +7,10 @@ const Introduction = () => {
     <div className="grid grid-cols-2 px-5 md:px-20 lg:px-32 my-10 md:my-16 lg:my-24">
       <Fade bottom>
         <div className="col-span-1 flex justify-center items-center">
-          <div className="w-40 md:w-60 lg:w-80 lg:h-80 bg-cover bg-center rounded-full"
+          <div className="w-32 h-32 md:w-60 md:h-60 lg:w-80 lg:h-80 bg-cover bg-center rounded-full"
             style={{ backgroundImage: `url(${img82})` }}
           >
           </div>
-          {/* <img className="w-40 md:w-60 lg:w-80" src={bangunIntroduction}></img> */}
         </div>
         <div className="col-span-1 flex items-center justify-start">
           <p className="text-justify md:text-right text-lg md:text-2xl lg:text-3xl">
